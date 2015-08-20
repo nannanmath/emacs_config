5864c2d5eb93ded7c706defccb18f8636a0cfce0
zhangna1 <nan1.zhang@intel.com>
2015-08-20 18:51:20 +0800

    commit emacs config file.

---
+++ custom/setup-company.el
@@ -0,0 +1,9 @@
+;; package: company
+(provide 'setup-company)
+(require 'company)
+
+(add-to-list 'company-backends 'company-semantic)
+(setq company-backends (delete 'company-clang company-backends))
+(define-key c-mode-map  [(control tab)] 'company-complete)
+(define-key c++-mode-map  [(control tab)] 'company-complete)
+(add-to-list 'company-backends 'company-c-headers)
